5fb91b6ba7258fc1a5cb8edebf760b06dc9ce2c0
Piyush Kumar <kumarpiyush2k6@gmail.com>
2026-05-12 00:15:37 +0530

    fix: change default isPublic to true in EventForm to ensure new events are visible

--- apps/admin/src/components/EventForm.jsx
+++ apps/admin/src/components/EventForm.jsx
@@ -63,7 +63,7 @@ const EventForm = ({ initialData = null, onSubmit, loading, onThemeChange }) => 
       displayMode: 'light',
       backgroundVideoUrl: ''
     },
-    isPublic: false,
+    isPublic: true,
     listingPaid: false,
     isTBA: false,
     isOnline: false,
